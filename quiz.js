1b5e2a1725c075915b6cc28e25d2aa26cc00a54f
Laloo <lelisahailu22@gmail.com>
2025-09-29 11:39:40 +0300

    document formatted

--- quiz.js
+++ quiz.js
@@ -1,14 +1,14 @@
-const submitBtn = document.getElementById('submit-answer');
-const feedback = document.getElementById('feedback');
+const submitBtn = document.getElementById("submit-answer");
+const feedback = document.getElementById("feedback");
 
-function checkAnswer(){
-let correctAnswer = "4";
-const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-if(correctAnswer === userAnswer){
-feedback.textContent = "Correct! Well done."
-}else{
+function checkAnswer() {
+  let correctAnswer = "4";
+  const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+  if (userAnswer === correctAnswer) {
+    feedback.textContent = "Correct! Well done.";
+  } else {
     feedback.textContent = "That's incorrect. Try again!";
-}
+  }
 }
 
-submitBtn.addEventListener('click' , checkAnswer);
+submitBtn.addEventListener("click", checkAnswer);
