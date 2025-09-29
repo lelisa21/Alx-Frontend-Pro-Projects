25eaafd74dfd211e51aed888c6a07d96439a7e0d
Laloo <lelisahailu22@gmail.com>
2025-09-29 11:34:28 +0300

    first commit

---
+++ quiz.js
@@ -0,0 +1,14 @@
+const submitBtn = document.getElementById('submit-answer');
+const feedback = document.getElementById('feedback');
+
+function checkAnswer(){
+let correctAnswer = "4";
+const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+if(correctAnswer === userAnswer){
+feedback.textContent = "Correct! Well done."
+}else{
+    feedback.textContent = "That's incorrect. Try again!";
+}
+}
+
+submitBtn.addEventListener('click' , checkAnswer);
